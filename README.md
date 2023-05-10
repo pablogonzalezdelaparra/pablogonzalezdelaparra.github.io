54f62e45a51b05cec7a8629a0b9471fbcb101036
Pablo González de la Parra <a01745096@itesm.mx>
2023-05-09 21:20:02 -0600

    Cambios en general

--- README.md
+++
@@ -1,1 +0,0 @@
-# pablogonzalezdelaparra.github.io
--- scrollDownButton.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.scroll-down-button').addEventListener('click', function() {
-    window.scrollBy({
-      top: window.innerHeight,
-      behavior: 'smooth'
-    });
-  });
-  
